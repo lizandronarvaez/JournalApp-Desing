d1be8ecbad222a26ff10102006b5c1f37241d449
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-11-23 10:28:09 +0100

    Componente principal journal

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,9 +1,70 @@
-import { SaveOutlined } from "@mui/icons-material";
-import { Button, Grid, TextField, Typography } from "@mui/material";
-import React from "react";
+import { DeleteOutline, SaveOutlined, UploadFileOutlined, UploadOutlined } from "@mui/icons-material";
+import { Button, Grid, IconButton, TextField, Typography } from "@mui/material";
+import React, { useEffect, useMemo, useRef } from "react";
 import ImageGallery from "./ImageGallery";
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from "../../hooks/useForms.js"
+import { addNewEmptyNote, setActiveNote } from "../../store/journal/journalSlice.js";
+import { startDeletingNote, startUpdateNote, startUploadingFiles } from "../../store/journal/thunks.js";
+import Swal from "sweetalert2/dist/sweetalert2.all.js";
 
 const NoteView = () => {
+
+  const dispatch = useDispatch();
+  const fileInputRef = useRef();
+  const { messageSave, active: note, isSaving } = useSelector(state => state.journal);
+  const { id, body, title, date, onInputChange, formState, onResetForm } = useForm(note);
+
+  const dateString = useMemo(() => {
+    const newDateFormat = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+    return new Date(date).toLocaleDateString('es-ES', newDateFormat);
+  }, [date])
+
+  const onSubmitNote = () => {
+    if (note) dispatch(startUpdateNote());
+
+    dispatch(addNewEmptyNote({ formState }));
+    onResetForm()
+  }
+
+  const onChangeInputFile = ({ target }) => {
+    const { files } = target;
+    if (files.length === 0) return;
+
+    dispatch(startUploadingFiles(files));
+  }
+
+  const deleteNote = () => {
+    Swal.fire({
+      title: "Borrar nota",
+      text: "Desea borrar la nota?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Eliminar nota",
+      cancelButtonText: "Cancelar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        dispatch(startDeletingNote())
+      }
+    });
+  }
+  useEffect(() => {
+    dispatch(setActiveNote(formState));
+  }, [formState])
+
+  useEffect(() => {
+    if (messageSave.length > 0) {
+      Swal.fire({
+        title: `Nota: ${messageSave}`,
+        text: "Actualizada correctamente",
+        icon: "success"
+      });
+    }
+  }, [messageSave]);
+
   return (
     <>
       <Grid
@@ -13,15 +74,43 @@ const NoteView = () => {
         sx={{ mb: 1 }}
       >
         <Grid item>
-          <Typography fontSize={39} fontWeight="ligth">
-            28 de Agosto, 2023
+          <Typography
+            fontSize={18}
+            fontWeight="ligth"
+            textTransform={"capitalize"}>
+            Fecha Creación Nota: {dateString}
           </Typography>
         </Grid>
         <Grid item>
-          <Button color="primary" sx={{ padding: 3 }}>
-            <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
+          <Button
+            disabled={isSaving}
+            color="primary"
+            sx={{ padding: 3 }}
+            onClick={onSubmitNote}
+          >
+            <SaveOutlined
+              sx={{
+                fontSize: 30,
+                mr: 1
+              }} />
             Guardar
           </Button>
+          <input
+            type="file"
+            onChange={onChangeInputFile}
+            style={{ display: "none" }}
+            ref={fileInputRef}
+            multiple
+          />
+          <IconButton
+            color="primary"
+            disabled={isSaving}
+            onClick={() => fileInputRef.current.click()}
+          >
+            <UploadOutlined>
+
+            </UploadOutlined>
+          </IconButton>
         </Grid>
         <Grid container>
           <TextField
@@ -31,6 +120,9 @@ const NoteView = () => {
             placeholder="Ingrese un titulo"
             label="Titulo"
             sx={{ border: "none", mb: 1 }}
+            value={title}
+            name="title"
+            onChange={onInputChange}
           />
           <TextField
             type="text"
@@ -39,10 +131,27 @@ const NoteView = () => {
             multiline
             placeholder="¿Que sucedió hoy?"
             minRows={5}
+            value={body}
+            name="body"
+            onChange={onInputChange}
           />
         </Grid>
+        <Grid container justifyContent="end">
+          <Button>
+            <DeleteOutline
+              sx={{ mt: 2 }}
+              color="error"
+              onClick={deleteNote}
+            >
+              Eliminar nota
+            </DeleteOutline>
+          </Button>
+        </Grid>
         {/* Galeria de imagenes */}
-        <ImageGallery />
+        <ImageGallery
+          key={note.id}
+          images={note.imageUrls}
+        />
       </Grid>
     </>
   );
---
+++ src/journal/views/index.js
@@ -0,0 +1,9 @@
+import NoteView from "./NoteView";
+import NothingSelectedView from "./NothingSelectedView";
+import ImageGallery from "./ImageGallery";
+
+export {
+    NoteView,
+    NothingSelectedView,
+    ImageGallery
+}
