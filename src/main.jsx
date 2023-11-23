37b981a0f28d465be24be816b0751bba0d571d34
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-11-23 10:28:45 +0100

    Aplicando un proveedor y store de react-redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import JournalApp from './JournalApp.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <JournalApp />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <JournalApp />
+    </BrowserRouter>
+  </Provider>
+
+
 )
